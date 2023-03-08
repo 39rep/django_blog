5233a4ab6a5bfbb68927dd8342b37f060a9986cb
39rep <okome3317@gmail.com>
2023-03-09 01:07:44 +0900

    Make post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from blog.views import PostList
+from blog.views import PostDetail
 
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 from blog.models import Post
 
@@ -9,3 +10,9 @@ class PostList(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    context_object_name = 'post'
