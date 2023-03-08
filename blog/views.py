9a707a2ffd6f0b76a27657265e8a6c56358decfd
39rep <okome3317@gmail.com>
2023-03-09 02:24:33 +0900

    add publish or private setting

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.core.exceptions import PermissionDenied
 
 from blog.models import Post
 
@@ -16,3 +17,10 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'blog/post.html'
     context_object_name = 'post'
+
+    def get_object(self):
+        post = super().get_object()
+        if post.is_published or self.request.user.is_authenticated:
+            return post
+        else:
+            raise PermissionDenied
